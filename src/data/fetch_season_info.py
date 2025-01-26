682694894d0d0433259fbeffb01ef146ea065585
Dana Ma <dana.ma537@gmail.com>
2025-01-26 21:56:00 +1100

    test: commit failing season info test

--- src/data/fetch_season_info.py
+++ src/data/fetch_season_info.py
@@ -3,15 +3,20 @@ import requests
 #TODO: move to consts file
 COMPETITION_ID = 1
 
-def fetch_season_info():
+def _fetch_season_info():
     result = requests.get(f"https://aflapi.afl.com.au/afl/v2/competitions/{COMPETITION_ID}/compseasons?pageSize=100")
-#    import pdb; pdb.set_trace()
     return result.json()
 
-def parse_json():
+def _parse_json(season_info):
+    print(season_info)
+    raise NotImplementedError
+
+def fetch_season_id(year):
+    season_info = _fetch_season_info()
+    _parse_json(season_info)
     raise NotImplementedError
 
 if __name__ == "__main__":
-    result = fetch_season_info()
+    result = _fetch_season_info()
     print(result)
     
