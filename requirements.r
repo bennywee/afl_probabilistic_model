81d640cd515af299c3ba9f0a9c3626a573dc4b64
Benjamin Wee <benw.cy93@gmail.com>
2025-02-02 18:00:16 +1100

    typo pkg name

--- requirements.r
+++ requirements.r
@@ -2,4 +2,4 @@ install.packages("fitzRoy")
 install.packages("future.apply")
 install.packages("languageserver") # For VScode env
 install.packages("yaml")
-install.packages("testhat")
+install.packages("testthat")
