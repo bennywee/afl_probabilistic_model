4ca92cca2d63b79e603bcdc655f9a0a0d97f3566
Benjamin Wee <benw.cy93@gmail.com>
2025-02-08 03:18:35 +0000

    git env variable

--- scripts/data_pull.r
+++ scripts/data_pull.r
@@ -1,10 +1,12 @@
 source("R/data_scrape.r")
 
 config <- yaml::read_yaml("configs/data_config.yaml")[["raw_ladder"]]
+git_branch <- system("git rev-parse --abbrev-ref HEAD")
+env <- if(git_branch == "main") "prod" else "dev"
 
 year_vector = config[["year_lb"]]:config[["year_ub"]]
 round_vector = config[["round_lb"]]:config[["round_ub"]]
-data_output_loc = config[["ladder_data_loc"]]
+data_output_loc = paste0("data/", env, "/", config[["ladder_data_loc"]])
 
 available_cores = parallel::detectCores()
 future::plan(future::multisession, workers = available_cores)
