42916a370eef61c0c070706378ab7298ff2daf60
Benjamin Wee <benw.cy93@gmail.com>
2025-02-09 11:23:52 +0000

    Refactored and fixed ladder scrape

--- scripts/data_pull.r
+++ scripts/data_pull.r
@@ -4,8 +4,8 @@ config <- yaml::read_yaml("configs/data_config.yaml")[["raw_ladder"]]
 git_branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
 env <- if(git_branch == "main") "prod" else "dev"
 
-year_vector = config[["year_lb"]]:config[["year_ub"]]
-round_vector = config[["round_lb"]]:config[["round_ub"]]
+years = config[["year_lb"]]:config[["year_ub"]]
+rounds = config[["round_lb"]]:config[["round_ub"]]
 data_output_loc = paste0("data/", env, "/", config[["ladder_data_loc"]])
 
 available_cores = parallel::detectCores()-1
@@ -15,11 +15,11 @@ if(!dir.exists(data_output_loc)){
     dir.create(data_output_loc, recursive = TRUE)
 }
 
-years = sort(rep(year_vector, length(round_vector)))
-rounds = rep(round_vector, length(year_vector))
+Map(function(season_vector) 
+    tryCatch(scrape_write_ladder(season = season_vector, 
+                                 round = rounds, 
+                                 output_path = data_output_loc), 
+    error = function(e){print(e); return(NULL)}), 
+    season_vector = years
+)
 
-future.apply::future_Map(function(x, y) 
-                         tryCatch(scrape_ladder(season = x, round = y, path = data_output_loc), 
-                         error = function(e){print(e); return(NULL)}),
-                         x = years, y = rounds)
- 
