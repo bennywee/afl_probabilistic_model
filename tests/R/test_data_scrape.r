04a7276c6157c404bdc3d289ea9d7d24fd26b003
Benjamin Wee <benw.cy93@gmail.com>
2025-02-09 11:24:04 +0000

    Test multiple rounds per parquet

--- tests/R/test_data_scrape.r
+++ tests/R/test_data_scrape.r
@@ -2,9 +2,14 @@ testthat::test_that("Ladder data pulled", {
     source("../../R/data_scrape.r")
     test_loc = "tests/R/temp"
     dir.create(test_loc, recursive = TRUE)
-    scrape_ladder(season = 2024, round = 1, path = test_loc)
+    scrape_write_ladder(season = 2024, round = c(1,2), output_path = test_loc)
     files_created = length(list.files(paste0(test_loc,"/Season=2024")))
+    
+    df <- arrow::read_parquet(paste0(test_loc, "/Season=2024/part-0.parquet"))
+    number_rounds <- length(unique(df[["Round.Number"]]))
+
     unlink(test_loc, recursive = TRUE)
 
     testthat::expect_equal(files_created, 1)
+    testthat::expect_equal(number_rounds, 2)
 })
