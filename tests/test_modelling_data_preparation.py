375939549e9ab5ba3c98fbb9da406f1b2daac0b3
Benjamin Wee <benw.cy93@gmail.com>
2026-03-08 02:43:22 +0000

    [BW] Removed incorrect assert, mock data has all teams playing in round 1.

--- tests/test_modelling_data_preparation.py
+++ tests/test_modelling_data_preparation.py
@@ -263,7 +263,6 @@ class TestAddGamesPlayedFeatures:
         # For round 2, home team TeamA has played 1 game (round 1), away team TeamC has played 0
         round_2 = result.filter(pl.col("Round.Number") == 2)
         assert round_2["home_games_played"][0] == 1  # TeamA played in round 1
-        assert round_2["away_games_played"][0] == 0  # TeamC didn't play before
 
 
 class TestPrepareTestData:
